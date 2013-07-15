7c279ecfd08f55530d3fe7df16fa8b3bf9cac20f
greg <greg@toki.waseda.jp>
2013-07-16 06:08:41 +0900

    logging in/out stuff etc

--- static/bbs.js
+++ static/bbs.js
@@ -2,6 +2,7 @@ var bbsApp = angular.module('bbs', []);
 
 bbsApp.run(function($rootScope) {
 	$rootScope.currentServer = null;
+	$rootScope.loginRedirect = null;
 });
 
 bbsApp.config(function($routeProvider) {
@@ -13,7 +14,7 @@ bbsApp.config(function($routeProvider) {
 		otherwise({redirectTo: '/servers'});
 });
 
-function BBS(url, $http, $rootScope) {
+function BBS(url, $http, $rootScope, $location) {
 	this.url = url;
 	this.name = url;
 	this.desc = "Pinging...";
@@ -21,6 +22,7 @@ function BBS(url, $http, $rootScope) {
 	this.lists = [];
 
 	this.loggedIn = false;
+	this.username = null;
 	this.session = null;
 	this.requiresLogin = false;
 
@@ -33,15 +35,15 @@ function BBS(url, $http, $rootScope) {
 		$http.post(self.url, cmd).success(function(recv) {
 			$rootScope.$broadcast("#" + recv.cmd, {
 				server: self,
-				data: recv,
-				error: false
+				data: recv
 			});
 		}).error(function(recv) {
-			$rootScope.$broadcast("#" + recv.cmd, {
+			console.log("error: " + recv.wrt);
+			$rootScope.$broadcast("!" + recv.wrt, {
 				server: self,
-				data: recv,
-				error: true
+				data: recv
 			});
+			console.log(recv);
 		});
 	}
 
@@ -67,6 +69,7 @@ function BBS(url, $http, $rootScope) {
 			session: self.session,
 			loggedIn: self.loggedIn,
 			requiresLogin: self.requiresLogin,
+			username: self.username
 		});
 	}
 
@@ -79,12 +82,33 @@ function BBS(url, $http, $rootScope) {
 		self.session = b.session;
 		self.loggedIn = b.loggedIn;
 		self.requiresLogin = b.requiresLogin;
+		self.username = b.username;
 	}
 
 	this.hello = function() {
 		self.send({
 			cmd: "hello"
 		});
+	}
+
+	this.login = function(username, password) {
+		self.send({
+			cmd: "login",
+			username: username,
+			password: password,
+			version: 0
+		});
+	}
+
+	this.logout = function() {
+		if (self.loggedIn) {
+			self.send({
+				cmd: "logout"
+			});
+		}	
+		self.loggedIn = false;
+		self.session = false;
+		self.username = null;
 	}
 
 	this.list = function(type, query, token) {
@@ -110,16 +134,39 @@ function BBS(url, $http, $rootScope) {
 		}
 		return "/threads";
 	}
+
+	// our log-in is bad
+	$rootScope.$on("!session", function(nm, evt) {
+		console.log(self);
+		if (evt.server == self) {
+			self.loggedIn = false;
+			self.session = null;
+			$rootScope.loginRedirect = $location.path();
+			$location.path("/login");
+		}
+	});
+
+	$rootScope.$on("#welcome", function(nm, evt) {
+		if (evt.server == self) {
+			self.loggedIn = true;
+			self.session = evt.data.session;
+			self.username = evt.data.username;
+
+			if (self == $rootScope.currentServer) {
+				localStorage["current"] = self.serialize();
+			}
+		}
+	});
 }
 
-bbsApp.factory('Servers', function($rootScope, $http) {
-	var defaultServer = new BBS("/bbs", $http, $rootScope);
+bbsApp.factory('Servers', function($rootScope, $http, $location) {
+	var defaultServer = new BBS("/bbs", $http, $rootScope, $location);
 	var servers = {};
 	servers[defaultServer.url] = defaultServer;
 
 	var Servers = {
-		add: function(server) {
-			servers.push(new BBS(server, $http, $rootScope));
+		add: function(url) {
+			servers[url] = new BBS(url, $http, $rootScope, $location);
 		},
 		list: function() {
 			var list = [];
@@ -152,6 +199,8 @@ bbsApp.filter('ago', function() {
 })
 
 bbsApp.run(function ($rootScope, Servers) {
+	Servers.add("/4chan");
+	Servers.add("/eti");
 	if (localStorage["current"]) {
 		var data = angular.fromJson(localStorage["current"]);
 		var srv = Servers.get(data.url);
@@ -173,15 +222,43 @@ function ServersCtrl($rootScope, $scope, $location, Servers) {
 		$location.path(srv.home());
 	}
 
+	$scope.logout = function() {
+		$rootScope.currentServer.logout();
+		$rootScope.currentServer = null;
+		delete localStorage["current"];
+	}
+
 	$scope.$on("#hello", function(type, evt) {
-		$scope.servers = Servers.list();
+		if (!evt.error) {
+			$scope.servers = Servers.list();
+		}
 	});
 
 	$scope.refresh();
 }
 
-function LoginCtrl() {
+function LoginCtrl($rootScope, $scope, $location) {
+	$scope.error = null;
+	$scope.username = "";
+	$scope.password = "";
 
+	$scope.login = function() {
+		$rootScope.currentServer.login($scope.username, $scope.password);
+	}
+
+	$scope.$on("#welcome", function(nm, evt) {
+		if ($rootScope.loginRedirect) {
+			$location.path($rootScope.loginRedirect);
+			$rootScope.loginRedirect = null;
+		} else {
+			$location.path(evt.server.home());
+		}
+	});
+
+	$scope.$on("!login", function(nm, evt) {
+		console.log(evt);
+		$scope.error = evt.data.msg;
+	});
 }
 
 function ThreadsCtrl($rootScope, $scope, $location, $routeParams) {
@@ -197,12 +274,16 @@ function ThreadsCtrl($rootScope, $scope, $location, $routeParams) {
 	}
 
 	$scope.$on("#list", function(nm, evt) {
-		if (!evt.error && evt.data.threads) {
+		if (evt.data.threads) {
 			$scope.threads = $scope.threads.concat(evt.data.threads);
 			$scope.next = evt.data.next || null;
-		} else {
-			$scope.error = evt.data.msg;
+			//console.log(evt);
 		}
+	});
+
+	$scope.$on("!list", function(nm, evt) {
+		console.log(evt);
+		$scope.error = evt.data.msg;
 	});
 
 	if (!$rootScope.currentServer) {
