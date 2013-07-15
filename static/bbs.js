c0a2bde8be3a0093bf8af3c6b4e79d1fc8aa6335
greg <greg@toki.waseda.jp>
2013-07-16 03:15:15 +0900

    server list

--- static/bbs.js
+++ static/bbs.js
@@ -1,30 +1,127 @@
 var bbsApp = angular.module('bbs', []);
 
+bbsApp.run(function($rootScope) {
+	$rootScope.currentServer = null;
+});
+
 bbsApp.config(function($routeProvider) {
 	$routeProvider.
 		when('/servers', {templateUrl: '/static/servers.html', controller: ServersCtrl}).
+		when('/login', {templateUrl: '/static/login.html', controller: LoginCtrl}).
+		when('/threads', {templateUrl: '/static/threads.html', controller: ThreadsCtrl}).
 		otherwise({redirectTo: '/servers'});
 });
 
-bbsApp.factory('Servers', function() {
-	var defaultServer = {
-		url: "/bbs",
-		name: "Default server"
+function BBS(url, $http, $rootScope) {
+	this.url = url;
+	this.name = url;
+	this.desc = "Pinging...";
+	this.access = {};
+
+	this.loggedIn = false;
+	this.session = null;
+	this.requiresLogin = false;
+
+	var self = this;
+
+	this.send = function(cmd) {
+		$http.post(self.url, cmd).success(function(recv) {
+			$rootScope.$broadcast("#" + recv.cmd, {
+				server: self,
+				data: recv,
+				error: false
+			});
+		}).error(function(recv) {
+			$rootScope.$broadcast("#" + recv.cmd, {
+				server: self,
+				data: recv,
+				error: true
+			});
+		});
 	}
-	var servers = [defaultServer];
-	return {
+
+	// update this bbs with data from "hello" cmd
+	this.refresh = function(data) {
+		self.name = data.name;
+		self.desc = data.desc;
+		self.access = data.access;
+		self.lists = data.lists || [];
+
+		if (self.access.user["get"] || self.access.user["list"]) {
+			self.requiresLogin = true;
+		}
+	} 
+
+	this.hello = function() {
+		self.send({
+			cmd: "hello"
+		});
+	}
+
+	this.home = function() {
+		if (self.requiresLogin && !self.loggedIn) {
+			return "/login";
+		}
+		if (self.lists.indexOf("board") != -1) {
+			return "/boards";
+		}
+		return "/list";
+	}
+}
+
+bbsApp.factory('Servers', function($rootScope, $http) {
+	var defaultServer = new BBS("/bbs", $http, $rootScope);
+	var servers = {};
+	servers[defaultServer.url] = defaultServer;
+
+	var Servers = {
 		add: function(server) {
-			servers.push(server);
+			servers.push(new BBS(server, $http, $rootScope));
 		},
 		list: function() {
-			return servers;
+			var list = [];
+			angular.forEach(servers, function(v, k) {
+				list.push(v);
+			});
+			return list;
 		},
 		refresh: function() {
-			// do this
+			angular.forEach(servers, function(srv) {
+				srv.hello();
+			});
 		}
 	};
+
+	$rootScope.$on("#hello", function(nm, evt) {
+		servers[evt.server.url].refresh(evt.data);
+	});
+
+	return Servers;
 });
 
-function ServersCtrl($scope, Servers) {
+function ServersCtrl($rootScope, $scope, $location, Servers) {
 	$scope.servers = Servers.list();
+	
+	$scope.refresh = function() {
+		Servers.refresh();
+	}
+
+	$scope.select = function(srv) {
+		$rootScope.currentServer = srv;
+		$location.path(srv.home());
+	}
+
+	$scope.$on("#hello", function(type, evt) {
+		$scope.servers = Servers.list();
+	});
+
+	$scope.refresh();
+}
+
+function LoginCtrl() {
+
+}
+
+function ThreadsCtrl() {
+	
 }
